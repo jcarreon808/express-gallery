3832333dcb2e37501051a7908a968980f78225b6
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2016-10-17 18:17:12 -1000

    adds Photo model

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+var db = require('./models');
+
+app.listen(3000, function() {
+  console.log('server started');
+  db.sequelize.sync();
+});
