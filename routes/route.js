af15c7034d33bc1f2a53d9c5cefe2534bd1d979e
Renee <rchar20@gmail.com>
2016-10-17 19:27:25 -1000

    implements gulp and routes partially working

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,98 @@
 const express = require('express');
 const pug = require('pug');
 const app = express();
+const Photo = require('../models').Photo;
 
+
+app.get('/', (req,res) => {
+  Photo.findAll()
+    .then(data =>{
+      //res.render the index view
+      res.json({data})
+    })
+})
+
+app.get('/gallery/:id',(req,res) => {
+  Photo.findById(req.params.id)
+    .then(data => {
+      //res.render the single photo view
+      res.json({data})
+    })
+})
+
+app.get('/gallery/new', (req,res) => {
+  res.render('new',{
+    data: 'something'
+  })
+})
+
+app.post('/gallery/new', (req,res) => {
+  Photo.create({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  })
+    .then(done => {
+      res.json({success:true});
+    })
+})
+
+app.post('/gallery', (req,res) => {
+  Photo.create({
+    author: req.body.author,
+    link: req.body.link,
+    description: req.body.description
+  })
+    .then(done => {
+      res.json({success:true});
+    })
+})
+
+app.get('/gallery/:id/edit', (req,res) =>{
+  Photo.findById(req.params.id)
+    .then(data => {
+      //render edit view
+      res.json({data})
+    })
+})
+
+app.post('/gallery/:id/edit', (req,res) =>{
+  Photo.findById(req.params.id)
+    .then(data => {
+      data.update({
+        author: req.body.author,
+        link: req.body.link,
+        description: req.body.description
+      })
+      .then(done => {
+        res.json({success: true})
+      })
+    })
+})
+
+app.put('/gallery/:id', (req,res) => {
+  Photo.findById(req.params.id)
+    .then(data => {
+      data.update({
+        author: req.body.author,
+        link: req.body.link,
+        description: req.body.description
+      })
+      .then(done => {
+        res.json({success: true})
+      })
+    })
+})
+
+
+app.delete('/gallery/:id', (req,res) => {
+  Photo.findById(req.params.id)
+    .then(data => {
+      data.destroy();
+    })
+    .then(done => {
+      res.json({success:true})
+    })
+})
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
 const bodyParser = require('body-parser');
 var app = express();
+var route = require('./routes/route.js');
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use('/',route);
+
+app.set('view engine','pug');
+app.set('views','./views');
+
 
 var db = require('./models');
 
-app.listen(3000, function() {
+app.listen(8080, function() {
   console.log('server started');
   db.sequelize.sync();
 });
 
+module.exports = db;
