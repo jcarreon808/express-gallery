7fd3b693ceecfbd114b5d87028c4c3f0bd99cb45
Jan Phillip Carreon <janphillipcarreon@yahoo.com>
2016-10-18 11:37:45 -1000

    update

--- models/Photo.js
+++ models/Photo.js
@@ -2,8 +2,8 @@ module.exports = function(sequelize, DataTypes) {
   var Photo = sequelize.define("Photo", {
     title:DataTypes.STRING,
     author: DataTypes.STRING,
-    link:DataTypes.STRING,
-    description:DataTypes.STRING
+    link:DataTypes.TEXT,
+    description:DataTypes.TEXT
   }, {
     classMethods: {
       associate: function(models) {
